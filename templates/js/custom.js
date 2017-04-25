420d842df989963f3cd27d267760811d581722fc
Aayush Sharma <aayushs96@gmail.com>
2017-04-24 20:07:29 -0500

    Add files via upload

--- templates/js/custom.js
+++ templates/js/custom.js
@@ -102,9 +102,9 @@ $(document).ready(function() {
   $(function(){
     jQuery(document).ready(function() {
     $('#home').backstretch([
-       "images/home-bg-slideshow1.jpg", 
-       "images/home-bg-slideshow2.jpg",
-       "images/home-bg-slideshow3.jpg",
+       "images/quad.jpg", 
+       "images/canopy.jpg",
+       "images/lion.png",
         ],  {duration: 2000, fade: 750});
     });
   })
